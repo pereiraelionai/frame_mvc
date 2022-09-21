81c8892ed07ae24d47d23ca45c721211cac4634b
pereiraelionai <pereira.elionai92@gmail.com>
2022-09-21 17:46:52 -0300

    implementando melhorias de reaproveitamento de código

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controllers;
 use MF\Controller\Action;
+use App\Models\Produto;
+
 
 class IndexController extends Action  {
 
 
     public function index() {
-        $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+        $this->view->dados = array('Sofá', 'Cadeira', 'Cama'); // Utilizar Container para retornar modelo do banco de dados
         $this->render('index.phtml', 'layout1');
     }
 
     public function sobreNos() {
-        $this->view->dados = ['Nootebook', 'Smartphone'];
+        $this->view->dados = ['Nootebook', 'Smartphone'];// Utilizar Container para retornar modelo do banco de dados
         $this->render('sobre_nos.phtml', 'layout2');
     }
 
